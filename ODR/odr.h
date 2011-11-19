196488d2cc2abce10581582bd273e7c9191f9628
Amelia E <ellison.amelia@gmail.com>
2011-11-18 23:15:49 -0500

    demux table works, haven't tested purge(), but that's minor

--- ODR/odr.h
+++ ODR/odr.h
@@ -11,9 +11,11 @@
 #include <sys/un.h>
 #include <time.h>
 
-#define ODR_SUNPATH "tmp/group15odr"
+#define ODR_SUNPATH "tmp/group15odr\0"
 #define ODR_PROTOCOL 32325
-#define TIME_SERV_SUNPATH "cse533_unixdg_path"
+#define TIME_SERV_SUNPATH "cse533_unixdg_path\0"
+#define	SUNPATH_ROOT	"group15\0"
+#define TIME_PORT 44444
 #define MAX_INTERFACES 10
 #define MAX_ROUTES 10
 #define MAXLINE 1024
@@ -22,3 +24,27 @@
 	#define max( a, b ) ( ((a) > (b)) ? (a) : (b) )
 #endif
 
+struct demux
+{
+	int 			port;
+	char			sun_path[30];
+	time_t			timestamp;
+	struct demux	*next;
+};
+
+struct route
+{
+	char			*nexthop_ip;
+	char 			nexthop_haddr[6];
+	int				index; 
+	int 			hops; 
+	time_t 			timestamp;
+};
+
+struct payload
+{
+	int 	srcport;
+	int 	destport;
+	int		msgsz;
+	char	*message;
+};
