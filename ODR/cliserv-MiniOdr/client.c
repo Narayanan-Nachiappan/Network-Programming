4e62e70bca8cd73486f43906727fb338ea3effab
YBizzle Kim <ybizzle.kim@gmail.com>
2011-11-20 11:04:44 -0500

    timeout

--- ODR/cliserv-MiniOdr/client.c
+++ ODR/cliserv-MiniOdr/client.c
@@ -2,8 +2,13 @@
 #include "sendrecv.c"
 #include <stdio.h>
 #include <stdlib.h>
+#include <signal.h>
+#include <setjmp.h>
 
 #define SIZE 1024
+
+jmp_buf env;
+extern void timeout();
 
 int main(int argc, char **argv){
 	err_msg("CSE 533 : Network Programming");
@@ -17,7 +22,7 @@ int main(int argc, char **argv){
 
 	int	sockfd;
 	int	tempfd;
-	char buf[24] = "GROUP15XXXXXX";
+	char buf[24] = "GROUP15_";
 	char vmAddr[INET_ADDRSTRLEN];
 	char hostName[SIZE];
 	struct sockaddr_un	cliaddr, servaddr;
@@ -32,12 +37,12 @@ int main(int argc, char **argv){
 	bzero(&cliaddr, sizeof(cliaddr));		/* bind an address for us */
 	cliaddr.sun_family = AF_LOCAL;
 	
-	tempfd = mkstemp(buf);
 	int random = ((rand() % 45535) +15000);
-	char temp[6];
-	sprintf(temp, "%d", random);
+	char temp[14];
+	sprintf(temp, "%d_XXXXXX", random);
 	strcat(buf, temp);
-	
+	tempfd = mkstemp(buf);
+
 	strcpy(cliaddr.sun_path, buf);
 
 	err_msg("Temporary File name created by mkstemp()");
@@ -55,7 +60,6 @@ int main(int argc, char **argv){
 	char *message="TIME_REQUEST";
 	int menu = 0;
 	
-
 	do{
 		printf("Choose the vm node (1 to 10): ");
 		scanf("%d", &menu);
@@ -69,14 +73,22 @@ int main(int argc, char **argv){
 		char vmName[4];
 		sprintf(vmName, "vm%d", menu);
 		hptr = gethostbyname(vmName);
-		pptr = hptr->h_addr_list;
+		pptr = (struct in_addr**) hptr->h_addr_list;
 		Inet_ntop(hptr->h_addrtype, *pptr, vmAddr, sizeof(vmAddr));
 
 		err_msg("%s Address: %s", vmName, vmAddr);
 		err_msg("client at node %s sending request to server at %s", hostName, vmName);
-	
-	//	dg_send_recv_cli( sockfd,vmAddr,444444,"Message",1);
-	msg_send(sockfd,vmAddr,SERVER_UNIX_PORT,message,0);
+
+		int i;
+		signal(SIGALRM, timeout);
+
+		if (setjmp(env) == 0) {
+			alarm(5);
+			msg_send(sockfd,vmAddr,SERVER_UNIX_PORT,message,0);
+			alarm(0);
+		} else {
+			msg_send(sockfd,vmAddr,SERVER_UNIX_PORT,message,1);
+		}
 	msg_recv(sockfd,message,vmAddr,SERVER_UNIX_PORT,1);
 
 	} while(menu > 0);
@@ -84,4 +96,12 @@ int main(int argc, char **argv){
 	unlink(buf);
 
 	exit(0);
+}
+
+void timeout(int sig){
+  int i;
+  signal(sig, SIG_IGN);
+  printf("Timeout.");
+  signal(SIGALRM, timeout);
+  longjmp(env, 1);
 }
