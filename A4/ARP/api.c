f1e27e9d1fe5a0cb5d7090c715bf2492697ae233
Nana <narayanan_ngl@yahoo.co.in>
2011-12-07 19:40:22 -0500

    API

--- A4/ARP/api.c
+++ A4/ARP/api.c
@@ -7,15 +7,27 @@
 #define SIZE 1024
 #define CLI_SUN_PATH "g15_api_path"
 #define ARP_DG_PATH "g15_arppath"
-jmp_buf env;
-extern void timeout();
+
+void printHW(char* ptr)
+{
+	int i = 6;
+	do 
+	{
+		printf("%.2x%s", *ptr++ & 0xff, (i == 1) ? " " : ":");
+	} while (--i > 0);
+}
 
 int areq (struct sockaddr *IPaddr, socklen_t sockaddrlen, struct hwaddr *HWaddr){
 	
 	int	sockfd;
 	int	tempfd;
 	int yes=1;
-	char sendline[100], temp2[50];
+	char sendline[100];
+	fd_set			rset;
+	struct timeval	tv;
+	tv.tv_sec = 5;
+	tv.tv_usec = 0;
+	char tmp[100];
 	char vmAddr[INET_ADDRSTRLEN];
 	char hostName[SIZE];
 	struct sockaddr_un	cliaddr, servaddr;
@@ -39,18 +51,18 @@ if (setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, &yes, sizeof(int)) == -1) {
 	bzero(&servaddr, sizeof(servaddr));
 	servaddr.sun_family = AF_LOCAL;
 	strcpy(servaddr.sun_path, ARP_DG_PATH);
-	signal(SIGALRM, timeout);
 	
-	strcpy(sendline, IPaddr->sa_data);
-	sprintf(temp2,",%d,%u,%c",HWaddr->sll_ifindex,HWaddr->sll_hatype,HWaddr->sll_halen);
-	strcat(sendline, temp2);
-	/*	strcat(sendline,IPaddr->sa_data);
-strcat(sendline,",");
-	strcat(sendline,itoa(HWaddr->sll_ifindex));
-	strcat(sendline,",");
-	strcat(sendline,itoa(HWaddr->sll_hatype));
-strcat(sendline,",");
-	strcat(sendline,itoa(HWaddr->sll_halen));*/
+	err_msg(IPaddr->sa_data);
+	strcpy(sendline,IPaddr->sa_data);
+	sprintf(tmp,",%d,%u,%c",HWaddr->sll_ifindex,HWaddr->sll_hatype,HWaddr->sll_halen);
+	strcat(sendline,tmp);
+							/*	strcat(sendline,IPaddr->sa_data);
+						strcat(sendline,",");
+							strcat(sendline,itoa(HWaddr->sll_ifindex));
+							strcat(sendline,",");
+							strcat(sendline,itoa(HWaddr->sll_hatype));
+						strcat(sendline,",");
+							strcat(sendline,itoa(HWaddr->sll_halen));*/
 	strcat(sendline,"\0");
 
  if (connect(sockfd,
@@ -60,42 +72,45 @@ strcat(sendline,",");
 		return -1;
  }
 
-//	if(sendto(sockfd, "hi", sizeof("hi"), 0, (struct sockaddr*) &servaddr, sizeof(struct sockaddr)) < 0)
-//	{
-//			printf("sendto error: %d %s\n", errno, strerror(errno));
-//		return -1;
-//	}
-Writen(sockfd, sendline, strlen(sendline));
-	if (setjmp(env) == 0) {
-			alarm(10);
-			recvfrom(sockfd, (struct hwaddr *)HWaddr, MAXLINE, 0, NULL, NULL);
-			alarm(0);
-	}
+							//	if(sendto(sockfd, "hi", sizeof("hi"), 0, (struct sockaddr*) &servaddr, sizeof(struct sockaddr)) < 0)
+							//	{
+							//			printf("sendto error: %d %s\n", errno, strerror(errno));
+							//		return -1;
+							//	}
+	err_msg("%s",sendline);
+	Writen(sockfd, sendline, strlen(sendline));
+	FD_ZERO(&rset);
+	FD_SET(sockfd, &rset);
+	int maxfd=sockfd;
+	select(maxfd+1, NULL, &rset, NULL, &tv);
+		if(FD_ISSET(sockfd,&rset)){
+			recvfrom(sockfd, (struct hwaddr *)HWaddr, sizeof(struct hwaddr), 0, NULL, NULL);
+			err_msg("read");
+			err_msg("IP is");
+			printHW(HWaddr->sll_addr);
+			err_msg("Index is %d",HWaddr->sll_ifindex);
+
+			}
 	else{
+		err_msg("API Timeout");
 		return -1;
 	}
-
 	unlink(CLI_SUN_PATH);
 	return 1;
 }
 
-void timeout(int sig){
-  int i;
-  signal(sig, SIG_IGN);
-  printf("Timeout.");
-  signal(SIGALRM, timeout);
-  longjmp(env, 1);
-}
 
-int main(int argc, char **agrv){
-	struct sockaddr * ipaddr = malloc(sizeof(struct sockaddr));
-	ipaddr->sa_family=1;
-	strcpy(ipaddr->sa_data,"192.168.1.102");
-	err_msg("%s",ipaddr->sa_data);
-	socklen_t len=sizeof(struct sockaddr);
-	struct hwaddr  *haddr = malloc(sizeof(struct hwaddr));
-	haddr->sll_ifindex=1;
-	haddr->sll_hatype=2;
-	haddr->sll_halen='1';
-	areq(ipaddr,len,haddr);
-}
+//int main(int argc, char **agrv){
+//	struct sockaddr * ipaddr;
+//	ipaddr=(struct sockaddr *) malloc(sizeof(struct sockaddr));
+//	ipaddr->sa_family=1;
+//	strcpy(ipaddr->sa_data,"192.168.1.102");
+//	err_msg("%s",ipaddr->sa_data);
+//	socklen_t len=sizeof(struct sockaddr);
+//	struct hwaddr  *haddr=malloc(sizeof(struct hwaddr));
+//	haddr->sll_ifindex=2;
+//	haddr->sll_hatype=1;
+//	haddr->sll_halen='6';
+//	int ret_val=areq(ipaddr,len,haddr);
+//	err_msg("retval %d" , ret_val);	
+//}
